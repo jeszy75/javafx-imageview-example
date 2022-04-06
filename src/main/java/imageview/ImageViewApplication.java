7303c42c77af48da2427984667ac0c94b1ab05c3
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2022-04-06 23:06:10 +0200

    Initial import

---
+++ src/main/java/imageview/ImageViewApplication.java
@@ -0,0 +1,33 @@
+package imageview;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ImageViewApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+//        Image image = new Image("http://cr.openjdk.java.net/~jeff/Duke/png/Hips.png");
+        Image image = new Image(getClass().getResourceAsStream("/Hips.png"));
+
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+        imageView.setFitHeight(400);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(imageView);
+
+        Scene scene = new Scene(root, 400, 600);
+        stage.setTitle("JavaFX ImageView Example");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
